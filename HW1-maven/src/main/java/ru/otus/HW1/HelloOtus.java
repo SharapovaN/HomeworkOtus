484a6a11a1a86d1183b3b3e69719b09d9f62bdb5
Natalia Sharapova <natali@tj-s.ru>
2019-10-06 12:38:10 +0300

    Add HW1-maven

---
+++ HW1-maven/src/main/java/ru/otus/HW1/HelloOtus.java
@@ -0,0 +1,10 @@
+package ru.otus.HW1;
+
+import com.google.common.collect.Lists;
+import java.util.*;
+
+class HelloOtus {
+    public static void main(String [] args) {
+        System.out.println(Lists.reverse(Arrays.asList("Otus","Hello")));
+    }
+}
